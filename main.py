90dc14082c24837dd25018964ad6e59b2b0e8f66
Abel Pinheiro de Figueiredo <a.figueiredo@portaltelemedicina.com.br>
2026-07-02 09:56:04 -0300

    add workflow and sample file

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+
+def main():
+    print("==================================")
+    print("Hello World - Projeto Demo PR Sync")
+    print("Versão: v1.0 - Estado Inicial")
+    print("==================================")
+
+if __name__ == "__main__":
+    main()
